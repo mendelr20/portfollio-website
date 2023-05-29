4c9b0a13060bab9b89b5892c5fe31a0e3895957a
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-05-29 14:44:44 -0400

    adding contact to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/projects">Projects</Link>
-       
+        <Link to="/contact">Contact</Link>
 
     </div>
   )

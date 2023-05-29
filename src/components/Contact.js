9a8c1c571ed6b0d78d67339f068dca16ce9ac2db
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-05-29 14:43:37 -0400

    adding Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import ContactForm
+ from './ContactForm'
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>Contact
+      <ContactForm/>
+    </div>
+
   )
 }
 

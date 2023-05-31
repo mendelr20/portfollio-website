5d5820a16b00a5836861ed968dd07a31842a1681
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-05-30 23:06:09 -0400

    adding skills to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,8 @@ function NavBar() {
 
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/projects">Skills</Link>
+        <Link to="/skillss">Skills</Link>
+        <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
 
     </div>

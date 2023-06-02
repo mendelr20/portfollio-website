8a449eeb19ce11cf546aba6aec9cbb1a05816325
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-02 08:05:38 -0400

    done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
+const trackingId = "G-KHFLY2E0DW"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
+ReactGA.set({
+  // userId: auth.currentUserId(),
+  // // any data that is relevant to the user session
+  // // that you would like to track with google analytics
+})
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter history={history}>
       <App />
   </BrowserRouter>,
   document.getElementById("root")

976118ba31fe4c761ce63ea3ece13db8a23fdd63
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-02 07:55:33 -0400

    final anyltics test

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-
-const RouteChangeTracker = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  }, [location]);
-
-  return <div></div>;
-};
-
-const trackingId = "G-KHFLY2E0DW"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <RouteChangeTracker />
-    <App />
+      <App />
   </BrowserRouter>,
   document.getElementById("root")
 );

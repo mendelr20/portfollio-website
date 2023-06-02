d0b9ed47cc05145d48b6d3de051d3ba18be8ee43
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-02 06:39:02 -0400

    adding google anylitics

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,16 @@ import About from "./About";
 import Contact from "./Contact";
 import Projects from "./Projects";
 import Skills from "./Skills";
+import ReactGA from 'react-ga';
+import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "G-KHFLY2E0DW"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: auth.currentUserId(),
+  // any data that is relevant to the user session
+  // that you would like to track with google analytics
+})
 
 function App() {
   return (
@@ -21,5 +31,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+export default ReactGA.withTracker(App);

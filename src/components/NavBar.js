bcccffe4cc417bf720b06240727bf3cf7a478139
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-05 21:25:49 -0400

    adding centered and styled NavBAr

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,29 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useLocation } from "react-router-dom";
 
 function NavBar() {
+  const location = useLocation();
+
   return (
     <Navbar bg="dark" variant="dark">
-      <Container >
-        <Nav className="ml-auto">
-          <LinkContainer to="/">
-            <Nav.Link>Home</Nav.Link>
+      <Container className="d-flex justify-content-center">
+        <Nav>
+          <LinkContainer to="/" exact>
+            <Nav.Link active={location.pathname === "/"} className="fs-5">Home</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/about">
-            <Nav.Link>About</Nav.Link>
+            <Nav.Link active={location.pathname === "/about"} className="fs-5">About</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/skills">
-            <Nav.Link>Skills</Nav.Link>
+            <Nav.Link active={location.pathname === "/skills"} className="fs-5">Skills</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/projects">
-            <Nav.Link>Projects</Nav.Link>
+            <Nav.Link active={location.pathname === "/projects"} className="fs-5">Projects</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/contact">
-            <Nav.Link>Contact</Nav.Link>
+            <Nav.Link active={location.pathname === "/contact"} className="fs-5">Contact</Nav.Link>
           </LinkContainer>
         </Nav>
       </Container>
@@ -31,45 +32,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-
-
-
-
-// import Container from "react-bootstrap/Container";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-// import { LinkContainer } from "react-router-bootstrap";
-
-// function NavBar() {
-//   return (
-//     <>
-//       <Navbar bg="dark" variant="dark">
-//         <Container>
-//           <LinkContainer className="ml-auto" to="/">
-//             <Navbar.Brand>Mendel Rosenblum</Navbar.Brand>
-//           </LinkContainer>
-//           <Nav className="ms-auto">
-//             <LinkContainer to="/">
-//               <Nav.Link>Home</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer to="/about">
-//               <Nav.Link>About</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer to="/skills">
-//               <Nav.Link>Skills</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer to="/projects">
-//               <Nav.Link>About</Nav.Link>
-//             </LinkContainer>
-//             <LinkContainer to="/contact">
-//               <Nav.Link>Contact</Nav.Link>
-//             </LinkContainer>
-//           </Nav>
-//         </Container>
-//       </Navbar>
-//     </>
-//   );
-// }
-
-// export default NavBar;

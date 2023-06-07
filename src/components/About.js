31cc8eefade47d0966b2dbf8801eb533ae28aea7
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-07 13:26:02 -0400

    adding styling to t he about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,33 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function About() {
   return (
-    <div className="d-flex justify-content-center align-items-start bg-black text-white py-3">
-      <h1>About Me</h1>
-      <p>
-        Hello, and welcome to my portfolio website! My name is Mendel Rosenblum,
-        and I am thrilled to share my journey with you. At the age of 17, I
-        embarked on an accelerated EMT course and graduated as the
-        valedictorian, marking the beginning of my career in emergency medical
-        services. I am currently a licensed NREMT (National Registry of
-        Emergency Medical Technicians) professional, certified as an EMT in both
-        New Jersey and New York. Over the past three years, I have had the
-        privilege of serving as an NYC 911 EMT, responding to emergencies and
-        providing critical care to those in need. It has been an incredibly
-        rewarding experience, allowing me to make a positive impact on people's
-        lives during their most vulnerable moments. In addition to my work in
-        New York City, I have dedicated the past two years to volunteering as an
-        EMT in New Jersey. The Township of Livingston recognized my commitment
-        and contribution to the community by awarding me the prestigious
-        Presidential Award. This recognition has further fueled my passion for
-        helping others and solidified my dedication to the field. Continuing my
-        education and professional growth, I am currently pursuing a NREMT
-        paramedic certification, scheduled to be completed by August 2023.
-        Alongside my studies, I have taken on the exciting challenge of
-        attending a software engineering bootcamp at Flarion, focusing on
-        developing my skills in the tech field. This pivot into technology
-        offers me a unique perspective and enables me to blend my passion for
-        healthcare with my growing interest in software engineering. Even as I
-        explore the realm of technology, emergency medicine remains a vital part
-        of who I am. I will continue to practice as a paramedic, considering it
-        not just a profession but also a personal hobby. This combination allows
-        me to apply my medical expertise while pursuing my enthusiasm for
-        technology. I am delighted to showcase my experiences, achievements, and
-        projects through this portfolio website. I hope you find inspiration in
-        my journey and witness the diverse skill set I bring to the table. Thank
-        you for visiting, and please feel free to reach out if you have any
-        questions or opportunities you'd like to discuss. Let's connect and make
-        a difference together.
-      </p>
+    <div className="bg-black text-white py-5">
+      <Container>
+        <h1 className="text-center mb-4">About Me</h1>
+        <p>
+          Hello, and welcome to my portfolio website! My name is Mendel Rosenblum, and I am thrilled to share my journey with you.
+        </p>
+        <p>
+          At the age of 17, I embarked on an accelerated EMT course and graduated as the valedictorian, marking the beginning of my career in emergency medical services.
+        </p>
+        <p>
+          I am currently a licensed NREMT (National Registry of Emergency Medical Technicians) professional, certified as an EMT in both New Jersey and New York. Over the past three years, I have had the privilege of serving as an NYC 911 EMT, responding to emergencies and providing critical care to those in need. It has been an incredibly rewarding experience, allowing me to make a positive impact on people's lives during their most vulnerable moments.
+        </p>
+        <p>
+          In addition to my work in New York City, I have dedicated the past two years to volunteering as an EMT in New Jersey. The Township of Livingston recognized my commitment and contribution to the community by awarding me the prestigious Presidential Award. This recognition has further fueled my passion for helping others and solidified my dedication to the field.
+        </p>
+        <p>
+          Continuing my education and professional growth, I am currently pursuing a NREMT paramedic certification, scheduled to be completed by August 2023. Alongside my studies, I have taken on the exciting challenge of attending a software engineering bootcamp at Flarion, focusing on developing my skills in the tech field. This pivot into technology offers me a unique perspective and enables me to blend my passion for healthcare with my growing interest in software engineering.
+        </p>
+        <p>
+          Even as I explore the realm of technology, emergency medicine remains a vital part of who I am. I will continue to practice as a paramedic, considering it not just a profession but also a personal hobby. This combination allows me to apply my medical expertise while pursuing my enthusiasm for technology.
+        </p>
+        <p>
+          I am delighted to showcase my experiences, achievements, and projects through this portfolio website. I hope you find inspiration in my journey and witness the diverse skill set I bring to the table. Thank you for visiting, and please feel free to reach out if you have any questions or opportunities you'd like to discuss. Let's connect and make a difference together.
+        </p>
+      </Container>
     </div>
   );
 }

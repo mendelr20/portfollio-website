1baa01bab0d65f81d80a29c0af391f2eb85fba28
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-06-15 07:49:38 -0400

    adding content

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
@@ -8,22 +9,22 @@ function HomePage() {
         
         <section>
           <h2>About Me</h2>
-          <p>I am a full stack Software Engineer with a passion for Emergency Medicine, Currently a EMT and soon to be a Paramedic .</p>
+          <p>I am a full-stack Software Engineer with a passion for Emergency Medicine. I am currently an NYC 911 EMT and soon to be a Paramedic.</p>
         </section>
         
         <section>
           <h2>Education</h2>
           <ul>
-            <li>Flatiron School, Full Stack Software Engineer, April 2023</li>
-            <li>NMETC (National Medical Education & Training Center), Paramedic, Expected Graduation, July 2023</li>
-            <li>B&H EMT (B&H Emergency Medical Training), EMT, August 2019</li>
+            <li><a href="https://www.flatironschool.com/">Flatiron School</a>, Full Stack Software Engineer, April 2023</li>
+            <li><a href="https://nmetc.com/">NMETC (National Medical Education & Training Center)</a>, Paramedic, Expected Graduation, July 2023</li>
+            <li><a href="https://www.bhemt.org/">B&H EMT (B&H Emergency Medical Training)</a>, NYS EMT, August 2019</li>
           </ul>
         </section>
         
         <section>
           <h2>Awards and Achievements</h2>
           <ul>
-            <li>Presidential Award - Bronze, Livingston Township / Hatzalah MetroWest, June 2023,</li>
+            <li><a href="https://presidentialserviceawards.gov/">Presidential Award - Bronze</a>, Livingston Township / Hatzalah MetroWest, June 2023</li>
             <li>Valedictorian, B&H EMT, August 2019</li>
           </ul>
         </section>
@@ -31,20 +32,27 @@ function HomePage() {
         <section>
           <h2>Work Experience</h2>
           <ul>
-            <li>Senior Care EMS, EMT, got promnoted to the NYC 911 divison under FDNY in November of 2020, January 2020 - Current</li>
+            <li><a href="https://seniorcareems.com/">Senior Care EMS</a>, EMT, promoted to the NYC 911 division under FDNY in November 2020, January 2020 - Present</li>
           </ul>
         </section>
         
         <section>
           <h2>Volunteer Experience</h2>
           <ul>
-            <li>Hatzalah MetroWest, EMT & Memebr of Documentation Commitie, Since its incepetion on September 3, 2021 throught current</li>
+            <li><a href="https://www.hatzalahmetrowest.org/">Hatzalah MetroWest</a>, EMT & Member of Documentation Committee, Since its inception on September 3, 2021, to Present</li>
           </ul>
         </section>
         
         <section>
           <h2>Contact Me</h2>
-          <p>Provide your contact information, such as email and LinkedIn profile.</p>
+          <p>Feel free to contact me through email or LinkedIn.</p>
+          <p>
+            Email: RosenblumMM@gmail.com<br />
+            LinkedIn: <a href="https://www.linkedin.com/in/mendel-rosenblum/">Your LinkedIn Profile</a>
+          </p>
+          <p>
+            <Link to="/contact">Contact Form</Link>
+          </p>
         </section>
         
       </div>
